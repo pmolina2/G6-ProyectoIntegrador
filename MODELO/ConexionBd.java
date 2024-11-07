a5763afe9d972eb4f27bbcad30300b074f5d8631
pmolina2 <pmolina2@udi.edu.co>
2024-11-06 21:01:59 -0500

    Add files via upload

--- MODELO/ConexionBd.java
+++ MODELO/ConexionBd.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package MODELO;
-
 import java.sql.*;
 import java.util.logging.*;
 
@@ -13,45 +12,40 @@ import java.util.logging.*;
  */
 
 
-/*
-    Clase ConexionBd encargada de realizar la conexión a la base de datos mediante el driverManager.
-*/
+
+//CLASE PADRE ConexionBd QUE HACE LA CONEXION Y SE EXTIENDE A TODAS LAS CLASES PARA RELIZAR LAS CONSULTAS
 public class ConexionBd {
 
-    /*  Se almacenan los valores de la url de la conexión, el usuario junto a su clave en el servidor Oracle en este caso
-        y la variable con que almacenará la conexión de la clase.
-    */
+    //VARIABLES DE LA CLASE
+
+    //ENCARGADA DE REALIZAR LA CONEXION, LOS DATOS SE OBTIENEN DEL ARCHIVO LISTENER.ORA EN LOS ARCHIVOS DE ORACLE
     private String url = "jdbc:oracle:thin:@localhost:1521:XE";
+
+    //USUARIO Y CONTRASEÑA DEL USUARIO DE LA BASE DE DATOS.
     private String usuario = "proyectoIntegradorpg6";
     private String contraseña = "pg6";
+
+    //OBJETO Connection con QUE ALMACENA LA CONEXION EN UNA VARIABLE, VALOR POR DEFECTO NULL.
     public Connection con = null;
 
-    
-    // Constructor de la clase que genera la conexión mediante los atributos anteriormente definidos
+    //CONSTRUCTOR DE LA CLASE
     public ConexionBd(){
+        
+    }
 
+    //METODO PARA OBTENER LA CONEXION
+    public Connection getConnection() {
         try {
-            con = DriverManager.getConnection(url, usuario, contraseña);
+            this.con = DriverManager.getConnection(url, usuario, contraseña);
         } catch (SQLException e) {
             System.out.println("Error " + e.getMessage());
         }
-
+        return this.con;
     }
 
-    // getter del atributo con (Connection)
-    public Connection getConnection() {
-        return con;
-    }
-
-    //método para cerrar la conexión de la clase y sus instancias, pasandole la conexion del momento como parámetro.
-    public void closeConnection(Connection con) {
+    //METODO PARA CERRAR LA CONEXION, ES RECOMENDABLE HACERLO DESPUES DE CADA CONSULTA PARA EVITAR ERRORES.
+    public void closeConnection() {
         
-        /*  
-            si la conexion es distinta a nula, es decir, si existe una conexión, se cierra 
-            mediante la función de la clase Connection .close().
-            Siempre que se hacen operaciones SQL se hace un try catch con un SQLException "var" para prevenir
-            posibles errores SQL
-        */
         if (con != null) {
             try {
                 con.close();
@@ -62,39 +56,5 @@ public class ConexionBd {
 
         }
     }
-
-    /*
-        Método principal de consulta de la clase, obteniendo todos los registros
-        de una tabla específica pasando su nombre como el parámetro "nombreTabla"
-    
-        throws SQLException se usa cuando la consulta devuelve un error SQL.
-        Básicamente sería el equivalente a usar el try catch con SQLException
-    */
-    public ResultSet consultarBd(String nombreTabla) throws SQLException {
-
-        PreparedStatement statement = con.prepareStatement("SELECT * FROM "+nombreTabla);
-        ResultSet res = statement.executeQuery();
-
-        return res;
-    }
-    
-    /*
-        Método para realizar consultas específicas. Con el fin de no sobre-
-        cargar la clase con métodos se simplificó todo en un método que pide
-        como parámetro la sentencia SQL a ejecutar desde el paquete controlador
-    */
-    public ResultSet consultarBdSentencia(String sentencia) throws SQLException{
-        
-        PreparedStatement statement = con.prepareStatement(sentencia);
-        ResultSet res = statement.executeQuery();
-        
-        return res;
-        
-    }
-    
-    /*
-        Todos los métodos de consulta devuelven un ResultSet que es un tipo
-        de dato incluido en la biblioteca SQL de java.
-    */
 
 }
---
+++ MODELO/UsuarioBd.java
@@ -0,0 +1,47 @@
+package MODELO;
+import java.util.*;
+import java.sql.*;
+
+public class UsuarioBd extends ConexionBd{
+    
+    public ArrayList<String> consultarUsuario(String nombreTabla, String cedula){
+
+        Connection conexion = this.getConnection();
+        
+        ArrayList<String> datosUsuario = new ArrayList<>();
+
+        String sentencia = String.format("""
+                SELECT *
+                FROM %s
+                WHERE cedula = ?
+                """, nombreTabla);
+        try {
+            
+            PreparedStatement statement = conexion.prepareStatement(sentencia);
+            statement.setString(1, cedula);
+            ResultSet resultset = statement.executeQuery();
+
+            if(resultset.next()){
+
+                datosUsuario.add(resultset.getString("cedula"));
+                datosUsuario.add(resultset.getString("nombreCompleto"));
+                datosUsuario.add(resultset.getString("direccion"));
+                datosUsuario.add(resultset.getString("telefono"));
+                datosUsuario.add(resultset.getString("correoElectronico"));
+                datosUsuario.add(resultset.getString("contrasenna"));
+
+            }
+
+            statement.close();
+            resultset.close();
+            closeConnection();
+            return datosUsuario;
+            
+        } catch(SQLException e){
+            System.out.println("Error "+e.getMessage());
+        }
+
+        return null;
+    }
+
+}
