021bfa5cbb59727d42d802ac6fa12ef7fdee0356
EdwinGutierrez1 <egutierrez11@udi.edu.co>
2024-11-15 17:09:45 -0500

    Pull de cambios finales de la rama controlador a la rama main.

--- Dominio/Pago.java
+++ Dominio/Pago.java
@@ -4,59 +4,50 @@ import java.util.Date;
 //Clase entidad que guarda la informacion de los asesores.
 public class Pago {
     //atributos necesarios para almacenar la info.
-    private int numPago, valorPago;
-    private String cedulaCliente, cedulaAsesor;
+    private String numTransferencia, metodoPago, valorPago, cedulaCliente, idCuota, cuotaPagada;
     private Date fecha;
 
     //Constructor para darle valores a los atributos.
-    public Pago(int numPago, int valorPago, String cedulaCliente, String cedulaAsesor, Date fecha) {
-        this.numPago = numPago;
+
+    public Pago(String numTransferencia, String metodoPago, String valorPago, Date fecha, String cedulaCliente, String idCuota, String cuotaPagada) {
+        this.numTransferencia = numTransferencia;
+        this.metodoPago = metodoPago;
         this.valorPago = valorPago;
         this.cedulaCliente = cedulaCliente;
-        this.cedulaAsesor = cedulaAsesor;
+        this.idCuota = idCuota;
+        this.cuotaPagada = cuotaPagada;
         this.fecha = fecha;
     }
 
-    //getters para obtener los datos de los atributos y setters para modificar sus datos.
-    public int getNumPago() {
-        return numPago;
+    public String getNumTransferencia() {
+        return numTransferencia;
     }
 
-    public void setNumPago(int numPago) {
-        this.numPago = numPago;
+    public String getMetodoPago() {
+        return metodoPago;
     }
 
-    public int getValorPago() {
+    public String getValorPago() {
         return valorPago;
-    }
-
-    public void setValorPago(int valorPago) {
-        this.valorPago = valorPago;
     }
 
     public String getCedulaCliente() {
         return cedulaCliente;
     }
 
-    public void setCedulaCliente(String cedulaCliente) {
-        this.cedulaCliente = cedulaCliente;
+    public String getIdCuota() {
+        return idCuota;
     }
 
-    public String getCedulaAsesor() {
-        return cedulaAsesor;
-    }
-
-    public void setCedulaAsesor(String cedulaAsesor) {
-        this.cedulaAsesor = cedulaAsesor;
+    public String getCuotaPagada() {
+        return cuotaPagada;
     }
 
     public Date getFecha() {
         return fecha;
     }
-
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
+    
+    
     
     
 }
